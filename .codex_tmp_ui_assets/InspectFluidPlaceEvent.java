8c935b9263e32877279a89a78d68ef16827d9b2a
QiuYe <qiuye2024github@163.com>
2026-05-30 22:06:37 +0800

    Fix jei int (#4)
    * 删除临时文件
    
    * 删除临时文件
    
    * fix

--- .codex_tmp_ui_assets/InspectFluidPlaceEvent.java
+++
@@ -1,9 +0,0 @@
-public class InspectFluidPlaceEvent {
-    public static void main(String[] args) throws Exception {
-        Class<?> c = Class.forName("net.neoforged.neoforge.event.level.BlockEvent$FluidPlaceBlockEvent");
-        System.out.println("CLASS " + c.getName());
-        for (var m : c.getDeclaredMethods()) {
-            System.out.println(m.toGenericString());
-        }
-    }
-}
--- src/main/java/com/fish_dan_/data_energistics/datagen/DataEnergisticsDataGenerator.java
+++ src/main/java/com/fish_dan_/data_energistics/datagen/DataEnergisticsDataGenerator.java
@@ -1,14 +1,14 @@
 package com.fish_dan_.data_energistics.datagen;
 
 import com.fish_dan_.data_energistics.Data_Energistics;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
-@EventBusSubscriber(modid = Data_Energistics.MODID, bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = Data_Energistics.MODID,value = Dist.CLIENT)
 public final class DataEnergisticsDataGenerator {
-    private DataEnergisticsDataGenerator() {
-    }
+    private DataEnergisticsDataGenerator() {}
 
     @SubscribeEvent
     public static void onGatherData(GatherDataEvent event) {
