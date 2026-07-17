2d2325076391a634d372ecc0b9c27eaae12cc282
QiuYe <qiuye2024github@163.com>
2026-07-17 22:36:05 +0800

    优化 Trinity CPU 下单计算性能 (#126)

--- src/main/java/com/fish_dan_/data_energistics/mixin/core/CraftConfirmMenuMixin.java
+++ src/main/java/com/fish_dan_/data_energistics/mixin/core/CraftConfirmMenuMixin.java
@@ -1,18 +1,29 @@
 package com.fish_dan_.data_energistics.mixin.core;
 
+import com.fish_dan_.data_energistics.common.crafting.trinity.TrinityDataCoreVirtualCpu;
 import com.fish_dan_.data_energistics.part.UniversalTerminalPart;
 import com.fish_dan_.data_energistics.util.UniversalTerminalHostAccessor;
 
 import net.minecraft.world.entity.player.Player;
 
+import appeng.api.networking.crafting.ICraftingCPU;
 import appeng.menu.me.crafting.CraftConfirmMenu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CraftConfirmMenu.class)
 public abstract class CraftConfirmMenuMixin {
+
+    @Inject(method = "cpuMatches", at = @At("HEAD"), cancellable = true, require = 1, remap = false)
+    private void dataEnergistics$excludeBusyTrinityWorker(ICraftingCPU cpu,
+                                                          CallbackInfoReturnable<Boolean> cir) {
+        if (cpu instanceof TrinityDataCoreVirtualCpu && cpu.isBusy()) {
+            cir.setReturnValue(false);
+        }
+    }
 
     @Inject(method = "startJob",
             at = @At(value = "INVOKE",
