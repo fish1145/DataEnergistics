f1b39fa63385db2e541f00fd3f15be56b28b654c
QiuYe <qiuye2024github@163.com>
2026-08-07 15:51:50 +0800

    [tower] 避免虚拟成员空转重建

--- src/main/java/com/fish_dan_/data_energistics/mixin/core/GridMixin.java
+++ src/main/java/com/fish_dan_/data_energistics/mixin/core/GridMixin.java
@@ -132,6 +132,11 @@ public abstract class GridMixin implements VirtualGridBridgeInternal {
         IGrid previousPrimary = this.dataEnergistics$virtualPrimaryGrid;
         Set<IGridNode> previousAll = dataEnergistics$identitySet(this.dataEnergistics$outgoingNodes);
         Set<IGridNode> previousActive = dataEnergistics$identitySet(this.dataEnergistics$outgoingActiveNodes);
+        if (previousPrimary == primaryGrid
+                && dataEnergistics$identitySetEquals(previousAll, normalizedAll)
+                && dataEnergistics$identitySetEquals(previousActive, normalizedActive)) {
+            return;
+        }
         this.dataEnergistics$virtualPrimaryGrid = primaryGrid;
         ArrayList<IGridNode> registeredNodes = new ArrayList<>();
         ArrayList<IGridNode> removedNodes = new ArrayList<>();
@@ -460,6 +465,19 @@ public abstract class GridMixin implements VirtualGridBridgeInternal {
         Set<IGridNode> result = Collections.newSetFromMap(new IdentityHashMap<>());
         result.addAll(nodes);
         return result;
+    }
+
+    @Unique
+    private static boolean dataEnergistics$identitySetEquals(Set<IGridNode> left, Set<IGridNode> right) {
+        if (left.size() != right.size()) {
+            return false;
+        }
+        for (IGridNode node : right) {
+            if (!left.contains(node)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Unique
