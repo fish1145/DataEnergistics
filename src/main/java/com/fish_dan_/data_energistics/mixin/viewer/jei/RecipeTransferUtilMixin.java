ec8b9915c6c75c04b272124d770bf1fe22efd5b3
QiuYe <qiuye2024github@163.com>
2026-08-08 00:08:44 +0800

    [jei] 独立通用传输注入

--- src/main/java/com/fish_dan_/data_energistics/mixin/viewer/jei/RecipeTransferUtilMixin.java
+++ src/main/java/com/fish_dan_/data_energistics/mixin/viewer/jei/RecipeTransferUtilMixin.java
@@ -1,4 +1,4 @@
-package com.fish_dan_.data_energistics.mixin.jei;
+package com.fish_dan_.data_energistics.mixin.viewer.jei;
 
 import com.fish_dan_.data_energistics.Data_Energistics;
 import com.fish_dan_.data_energistics.client.jei.transfer.JeiPatternTransferContextBridge;
