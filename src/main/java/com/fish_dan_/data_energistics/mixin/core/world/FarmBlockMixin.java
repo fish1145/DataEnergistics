d3861e1c3c262004411f53dfc7737dc303eaaa3a
QiuYe <qiuye2024github@163.com>
2026-08-09 13:21:13 +0800

    拆分世界行为 Mixin

--- src/main/java/com/fish_dan_/data_energistics/mixin/core/world/FarmBlockMixin.java
+++ src/main/java/com/fish_dan_/data_energistics/mixin/core/world/FarmBlockMixin.java
@@ -1,4 +1,4 @@
-package com.fish_dan_.data_energistics.mixin.core;
+package com.fish_dan_.data_energistics.mixin.core.world;
 
 import com.fish_dan_.data_energistics.world.PersistentFarmlandSavedData;
 
