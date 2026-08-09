5f883a08037345051b717fae3a15b31672f2c061
QiuYe <qiuye2024github@163.com>
2026-08-09 13:16:11 +0800

    拆分合成运行时 Mixin

--- src/main/java/com/fish_dan_/data_energistics/mixin/core/crafting/CraftingCPUMenuMixin.java
+++ src/main/java/com/fish_dan_/data_energistics/mixin/core/crafting/CraftingCPUMenuMixin.java
@@ -1,4 +1,4 @@
-package com.fish_dan_.data_energistics.mixin.core;
+package com.fish_dan_.data_energistics.mixin.core.crafting;
 
 import com.fish_dan_.data_energistics.common.crafting.trinity.execution.cpu.TrinityDataCoreVirtualCpu;
 
--- src/main/java/com/fish_dan_/data_energistics/mixin/core/crafting/CraftingCpuLogicVirtualCompletionMixin.java
+++ src/main/java/com/fish_dan_/data_energistics/mixin/core/crafting/CraftingCpuLogicVirtualCompletionMixin.java
@@ -1,4 +1,4 @@
-package com.fish_dan_.data_energistics.mixin.core;
+package com.fish_dan_.data_energistics.mixin.core.crafting;
 
 import com.fish_dan_.data_energistics.Data_Energistics;
 import com.fish_dan_.data_energistics.api.crafting.dispatch.VirtualCraftingCompletion;
