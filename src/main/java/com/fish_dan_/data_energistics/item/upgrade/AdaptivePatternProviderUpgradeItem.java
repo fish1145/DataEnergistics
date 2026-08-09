cf83d9b5e89e340a28f379a621bf3fda697e9d13
QiuYe <qiuye2024github@163.com>
2026-08-09 12:28:11 +0800

    拆分能力升级物品

--- src/main/java/com/fish_dan_/data_energistics/item/upgrade/AdaptivePatternProviderUpgradeItem.java
+++ src/main/java/com/fish_dan_/data_energistics/item/upgrade/AdaptivePatternProviderUpgradeItem.java
@@ -1,4 +1,4 @@
-package com.fish_dan_.data_energistics.item;
+package com.fish_dan_.data_energistics.item.upgrade;
 
 import com.fish_dan_.data_energistics.registry.DEBlockEntities;
 import com.fish_dan_.data_energistics.registry.DEBlocks;
