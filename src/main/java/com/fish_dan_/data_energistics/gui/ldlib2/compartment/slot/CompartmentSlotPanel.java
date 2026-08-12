081d72cc19c961126b0572d75ff562b14e01d6dd
QiuYe <qiuye2024github@163.com>
2026-08-12 22:38:36 +0800

    分类 LDLib2 隔室槽位组件

--- src/main/java/com/fish_dan_/data_energistics/gui/ldlib2/compartment/slot/CompartmentSlotPanel.java
+++ src/main/java/com/fish_dan_/data_energistics/gui/ldlib2/compartment/slot/CompartmentSlotPanel.java
@@ -1,4 +1,4 @@
-package com.fish_dan_.data_energistics.gui.ldlib2.compartment;
+package com.fish_dan_.data_energistics.gui.ldlib2.compartment.slot;
 
 import com.fish_dan_.data_energistics.Data_Energistics;
 import com.fish_dan_.data_energistics.gui.ldlib2.ae.bridge.AeItemSlot;
--- src/main/java/com/fish_dan_/data_energistics/gui/ldlib2/compartment/slot/MeOutputCompartmentPanel.java
+++ src/main/java/com/fish_dan_/data_energistics/gui/ldlib2/compartment/slot/MeOutputCompartmentPanel.java
@@ -1,4 +1,4 @@
-package com.fish_dan_.data_energistics.gui.ldlib2.compartment;
+package com.fish_dan_.data_energistics.gui.ldlib2.compartment.slot;
 
 import com.fish_dan_.data_energistics.gui.ldlib2.ae.bridge.AeMenuBridge;
 import com.fish_dan_.data_energistics.menu.CompartmentMenu;
