235b39408a3662d3c20f79f6397d79a310f8e0d0
QiuYe <qiuye2024github@163.com>
2026-08-15 17:03:37 +0800

    归类数据重组器客户端布局

--- src/main/java/com/fish_dan_/data_energistics/client/recipe/DataReassemblerLayout.java
+++ src/main/java/com/fish_dan_/data_energistics/client/recipe/DataReassemblerLayout.java
@@ -1,4 +1,4 @@
-package com.fish_dan_.data_energistics.client;
+package com.fish_dan_.data_energistics.client.recipe;
 
 import com.fish_dan_.data_energistics.Data_Energistics;
 import com.fish_dan_.data_energistics.recipe.reassembler.DataRipperReassemblerRecipe;
