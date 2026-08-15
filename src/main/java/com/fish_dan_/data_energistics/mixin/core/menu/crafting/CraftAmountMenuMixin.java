a3b3cf250a10fbe30b2ee76819f8fa09d74477ea
QiuYe <qiuye2024github@163.com>
2026-08-15 15:22:27 +0800

    分类服务端合成菜单 Mixin

--- src/main/java/com/fish_dan_/data_energistics/mixin/core/menu/crafting/CraftAmountMenuMixin.java
+++ src/main/java/com/fish_dan_/data_energistics/mixin/core/menu/crafting/CraftAmountMenuMixin.java
@@ -1,4 +1,4 @@
-package com.fish_dan_.data_energistics.mixin.core.menu;
+package com.fish_dan_.data_energistics.mixin.core.menu.crafting;
 
 import com.fish_dan_.data_energistics.Data_Energistics;
 import com.fish_dan_.data_energistics.common.crafting.trinity.planning.CraftingQuantityMode;
--- src/main/java/com/fish_dan_/data_energistics/mixin/core/menu/crafting/ResultSlotMixin.java
+++ src/main/java/com/fish_dan_/data_energistics/mixin/core/menu/crafting/ResultSlotMixin.java
@@ -1,4 +1,4 @@
-package com.fish_dan_.data_energistics.mixin.core.menu;
+package com.fish_dan_.data_energistics.mixin.core.menu.crafting;
 
 import com.fish_dan_.data_energistics.item.powered.PoweredCraftingEnergyHelper;
 
