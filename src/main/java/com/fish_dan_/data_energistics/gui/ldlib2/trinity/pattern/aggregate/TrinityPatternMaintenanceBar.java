298c5eb6cfa5bfd75bbc797fe0c4b846ad8b8df2
QiuYe <qiuye2024github@163.com>
2026-08-15 23:30:15 +0800

    居中样板管理进度条

--- src/main/java/com/fish_dan_/data_energistics/gui/ldlib2/trinity/pattern/aggregate/TrinityPatternMaintenanceBar.java
+++ src/main/java/com/fish_dan_/data_energistics/gui/ldlib2/trinity/pattern/aggregate/TrinityPatternMaintenanceBar.java
@@ -28,7 +28,7 @@ final class TrinityPatternMaintenanceBar extends BindableUIElement<TrinityPatter
         setId(id);
         layout(layout -> layout
                 .positionType(TaffyPosition.ABSOLUTE)
-                .left(0)
+                .left(1)
                 .top(15)
                 .width(WIDTH)
                 .height(HEIGHT));
