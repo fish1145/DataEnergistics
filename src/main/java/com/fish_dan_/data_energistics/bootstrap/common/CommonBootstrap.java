c70b4a13b0ba471886237589203295d39de9aa55
QiuYe <qiuye2024github@163.com>
2026-08-17 23:39:36 +0800

    提前注册配置以启用 Mod Menu 配置入口

--- src/main/java/com/fish_dan_/data_energistics/bootstrap/common/CommonBootstrap.java
+++ src/main/java/com/fish_dan_/data_energistics/bootstrap/common/CommonBootstrap.java
@@ -1,6 +1,9 @@
 package com.fish_dan_.data_energistics.bootstrap.common;
 
+import com.fish_dan_.data_energistics.Data_Energistics;
 import com.fish_dan_.data_energistics.common.trinity.host.TrinityDataCoreSyncAccessors;
+import com.fish_dan_.data_energistics.configuration.rules.schema.DataExtractorRulesConfiguration;
+import com.fish_dan_.data_energistics.configuration.schema.DataEnergisticsConfiguration;
 
 import net.neoforged.bus.api.IEventBus;
 
@@ -10,6 +13,10 @@ public final class CommonBootstrap {
 
     public static void init(IEventBus modEventBus) {
         TrinityDataCoreSyncAccessors.init();
+        Data_Energistics.LOGGER.debug(
+                "Registered Configuration schemas {} and {}",
+                DataEnergisticsConfiguration.HOLDER.getConfigId(),
+                DataExtractorRulesConfiguration.HOLDER.getConfigId());
         CommonProxy.init(modEventBus);
     }
 }
