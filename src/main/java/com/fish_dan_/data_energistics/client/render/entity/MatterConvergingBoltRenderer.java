a63d134c43cfb1f263a9c81f8336abdec0a19f2d
QiuYe <qiuye2024github@163.com>
2026-09-10 21:05:45 +0800

    弩模式弹体改用实际弹药模型渲染

--- src/main/java/com/fish_dan_/data_energistics/client/render/entity/MatterConvergingBoltRenderer.java
+++ src/main/java/com/fish_dan_/data_energistics/client/render/entity/MatterConvergingBoltRenderer.java
@@ -5,7 +5,6 @@ import com.fish_dan_.data_energistics.entity.projectile.MatterConvergingBoltEnti
 import com.fish_dan_.data_energistics.item.powered.MatterConvergingCrossbowMode;
 
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -14,7 +13,6 @@ import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.FastColor;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.inventory.InventoryMenu;
@@ -22,14 +20,11 @@ import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import org.joml.Vector3f;
 
 public class MatterConvergingBoltRenderer extends EntityRenderer<MatterConvergingBoltEntity> {
 
-    private static final ResourceLocation NORMAL_ARROW_LOCATION = ResourceLocation.withDefaultNamespace("textures/entity/projectiles/arrow.png");
-    private static final ResourceLocation TIPPED_ARROW_LOCATION = ResourceLocation.withDefaultNamespace("textures/entity/projectiles/tipped_arrow.png");
     private static final float AMMUNITION_MODEL_SIZE = 8.0F / 16.0F;
     private static final Direction[] MODEL_FACES = Direction.values();
     private final ItemRenderer itemRenderer;
@@ -51,40 +46,7 @@ public class MatterConvergingBoltRenderer extends EntityRenderer<MatterConvergin
             super.render(entity, entityYaw, partialTick, poseStack, buffer, packedLight);
             return;
         }
-        if (entity.firingMode() == MatterConvergingCrossbowMode.GRENADE) {
-            this.renderAmmunition(entity, poseStack, buffer, packedLight);
-            super.render(entity, entityYaw, partialTick, poseStack, buffer, packedLight);
-            return;
-        }
-        poseStack.pushPose();
-        poseStack.mulPose(Axis.YP.rotationDegrees(Mth.lerp(partialTick, entity.yRotO, entity.getYRot()) - 90.0F));
-        poseStack.mulPose(Axis.ZP.rotationDegrees(Mth.lerp(partialTick, entity.xRotO, entity.getXRot())));
-
-        poseStack.mulPose(Axis.XP.rotationDegrees(45.0F));
-        poseStack.scale(0.05625F, 0.05625F, 0.05625F);
-        poseStack.translate(-4.0F, 0.0F, 0.0F);
-
-        VertexConsumer consumer = buffer.getBuffer(RenderType.entityCutout(this.getTextureLocation(entity)));
-        PoseStack.Pose pose = poseStack.last();
-        int color = entity.getColor();
-        this.vertex(pose, consumer, -7, -2, -2, 0.0F, 0.15625F, -1, 0, 0, packedLight, color);
-        this.vertex(pose, consumer, -7, -2, 2, 0.15625F, 0.15625F, -1, 0, 0, packedLight, color);
-        this.vertex(pose, consumer, -7, 2, 2, 0.15625F, 0.3125F, -1, 0, 0, packedLight, color);
-        this.vertex(pose, consumer, -7, 2, -2, 0.0F, 0.3125F, -1, 0, 0, packedLight, color);
-        this.vertex(pose, consumer, -7, 2, -2, 0.0F, 0.15625F, 1, 0, 0, packedLight, color);
-        this.vertex(pose, consumer, -7, 2, 2, 0.15625F, 0.15625F, 1, 0, 0, packedLight, color);
-        this.vertex(pose, consumer, -7, -2, 2, 0.15625F, 0.3125F, 1, 0, 0, packedLight, color);
-        this.vertex(pose, consumer, -7, -2, -2, 0.0F, 0.3125F, 1, 0, 0, packedLight, color);
-
-        for (int i = 0; i < 4; ++i) {
-            poseStack.mulPose(Axis.XP.rotationDegrees(90.0F));
-            this.vertex(pose, consumer, -8, -2, 0, 0.0F, 0.0F, 0, 1, 0, packedLight, color);
-            this.vertex(pose, consumer, 8, -2, 0, 0.5F, 0.0F, 0, 1, 0, packedLight, color);
-            this.vertex(pose, consumer, 8, 2, 0, 0.5F, 0.15625F, 0, 1, 0, packedLight, color);
-            this.vertex(pose, consumer, -8, 2, 0, 0.0F, 0.15625F, 0, 1, 0, packedLight, color);
-        }
-
-        poseStack.popPose();
+        this.renderAmmunition(entity, poseStack, buffer, packedLight);
         super.render(entity, entityYaw, partialTick, poseStack, buffer, packedLight);
     }
 
@@ -141,19 +103,8 @@ public class MatterConvergingBoltRenderer extends EntityRenderer<MatterConvergin
         }
     }
 
-    private void vertex(PoseStack.Pose pose, VertexConsumer consumer, int x, int y, int z, float u, float v,
-                        int normalX, int normalY, int normalZ, int packedLight, int color) {
-        consumer.addVertex(pose, x, y, z)
-                .setColor(FastColor.ARGB32.red(color), FastColor.ARGB32.green(color), FastColor.ARGB32.blue(color), 255)
-                .setUv(u, v)
-                .setOverlay(OverlayTexture.NO_OVERLAY)
-                .setLight(packedLight)
-                .setNormal(pose, normalX, normalZ, normalY);
-    }
-
     @Override
     public ResourceLocation getTextureLocation(MatterConvergingBoltEntity entity) {
-        if (entity.firingMode() != MatterConvergingCrossbowMode.CROSSBOW) return InventoryMenu.BLOCK_ATLAS;
-        return entity.getColor() >= 0 ? TIPPED_ARROW_LOCATION : NORMAL_ARROW_LOCATION;
+        return InventoryMenu.BLOCK_ATLAS;
     }
 }
